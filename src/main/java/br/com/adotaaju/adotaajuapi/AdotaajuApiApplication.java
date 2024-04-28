f3ef408a1e6b022b20574f1005eca0bd72da7e2b
Reinan Kauã <contato.reinankaua@gmail.com>
2024-04-28 18:03:13 -0300

    first commit

---
+++ src/main/java/br/com/adotaaju/adotaajuapi/AdotaajuApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.adotaaju.adotaajuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotaajuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotaajuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adotaaju/adotaajuapi/AdotaajuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adotaaju.adotaajuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotaajuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
