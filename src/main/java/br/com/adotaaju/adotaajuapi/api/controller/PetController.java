d05467309296d2192c6322f56f344470f6e0af73
Arthur Fernando <arthurfernando18@gmail.com>
2024-06-07 17:19:59 -0300

    Implementação das mensagens de exceção e sucesso do response

--- src/main/java/br/com/adotaaju/adotaajuapi/api/controller/PetController.java
+++ src/main/java/br/com/adotaaju/adotaajuapi/api/controller/PetController.java
@@ -1,29 +1,25 @@
 package br.com.adotaaju.adotaajuapi.api.controller;
 
 import br.com.adotaaju.adotaajuapi.api.dto.PetDTO;
+import br.com.adotaaju.adotaajuapi.core.GenericResponse;
 import br.com.adotaaju.adotaajuapi.domain.service.PetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/pet")
@@ -38,9 +34,16 @@ public class PetController {
          @ApiResponse(responseCode = "400", description = "Invalid information provided")
    })
    @PostMapping(value = "/register")
-   public ResponseEntity<PetDTO.Response> create(@Valid @RequestBody PetDTO.Request petDTO) {
+   public ResponseEntity<GenericResponse<PetDTO.Response>> create(@Valid @RequestBody PetDTO.Request petDTO) {
       PetDTO.Response responseDTO = petService.save(petDTO);
-      return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+
+      GenericResponse<PetDTO.Response> successResponse = new GenericResponse<>(
+              HttpStatus.CREATED.value(),
+              "Operação realizada com sucesso.",
+              responseDTO
+      );
+
+      return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
    }
 
    @Operation(summary = "Search for pets using filter", description = "Search for pets according to the filter passed")
@@ -49,12 +52,25 @@ public class PetController {
          @ApiResponse(responseCode = "204", description = "There are no registered pets in database")
    })
    @GetMapping("/findGeneric")
-   public ResponseEntity<Page<PetDTO.Response>> searchByCriteria(@Valid PetDTO.Response petDTO, Pageable pageable) {
+   public ResponseEntity<GenericResponse<Page<PetDTO.Response>>> searchByCriteria(@Valid PetDTO.Response petDTO, Pageable pageable) {
+
       Page<PetDTO.Response> result = petService.searchByCriteria(petDTO, pageable);
+
       if (result.isEmpty()) {
-         return ResponseEntity.noContent().build();
+         GenericResponse<Page<PetDTO.Response>> errorResponse = new GenericResponse<>(
+                 HttpStatus.NO_CONTENT.value(),
+                 "Não há pets registrados no banco de dados.",
+                 result
+         );
+         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(errorResponse);
       }
-      return ResponseEntity.ok(result);
+
+      GenericResponse<Page<PetDTO.Response>> successResponse = new GenericResponse<>(
+              HttpStatus.OK.value(),
+              "Operação realizada com sucesso.",
+              result
+      );
+      return ResponseEntity.ok(successResponse);
    }
 
    @Operation(summary = "Update a pet", description = "Updates a pet in the system with the information provided")
@@ -63,15 +79,21 @@ public class PetController {
          @ApiResponse(responseCode = "404", description = "Couldn't find this pet to update")
    })
    @PutMapping("/update/")
-   public ResponseEntity<PetDTO.Response> update(
-         @RequestParam Long id,
-         @RequestBody PetDTO.Request petDTO) throws Exception {
+   public ResponseEntity<GenericResponse<PetDTO.Response>> update(@RequestParam Long id, @RequestBody PetDTO.Request petDTO)
+           throws Exception {
 
       PetDTO.Response responseDTO = petService.update(id, petDTO);
+
+      GenericResponse<PetDTO.Response> successResponse = new GenericResponse<>(
+              HttpStatus.OK.value(),
+              "Operação realizada com sucesso.",
+              responseDTO
+      );
+
       if (responseDTO == null) {
          return ResponseEntity.notFound().build();
       }
-      return ResponseEntity.ok(responseDTO);
+      return ResponseEntity.ok(successResponse);
    }
 
    @Operation(summary = "Delete a pet", description = "Deletes a pet in the system with the ID provided")
--- src/main/java/br/com/adotaaju/adotaajuapi/api/controller/TutorController.java
+++ src/main/java/br/com/adotaaju/adotaajuapi/api/controller/TutorController.java
@@ -2,6 +2,8 @@ package br.com.adotaaju.adotaajuapi.api.controller;
 
 import br.com.adotaaju.adotaajuapi.api.dto.TutorDTO;
 import br.com.adotaaju.adotaajuapi.domain.service.TutorService;
+import br.com.adotaaju.adotaajuapi.core.ErrorResponse;
+import br.com.adotaaju.adotaajuapi.core.GenericResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,9 +33,14 @@ public class TutorController {
             @ApiResponse(responseCode = "400", description = "Invalid information provided")
     })
     @PostMapping(value = "/register")
-    public ResponseEntity<TutorDTO.Response> create(@Valid @RequestBody TutorDTO.Request tutorDTO) {
+    public ResponseEntity<GenericResponse<TutorDTO.Response>> create(@Valid @RequestBody TutorDTO.Request tutorDTO) {
         TutorDTO.Response responseDTO = tutorService.save(tutorDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+        GenericResponse<TutorDTO.Response> successResponse = new GenericResponse<>(
+                HttpStatus.CREATED.value(),
+                "Operação realizada com sucesso.",
+                responseDTO
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(successResponse);
     }
 
     @Operation(summary = "Search for tutor by ID", description = "Search for a tutor using the provided ID")
@@ -43,12 +49,14 @@ public class TutorController {
             @ApiResponse(responseCode = "404", description = "Tutor not found")
     })
     @GetMapping("/findById/")
-    public ResponseEntity<TutorDTO.Response> findById(@RequestParam Long id) {
+    public ResponseEntity<?> findById(@RequestParam Long id) {
         try {
             TutorDTO.Response responseDTO = tutorService.findById(id);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "An error occurred while searching for the tutor: "
+                                + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
     }
 
@@ -57,13 +65,15 @@ public class TutorController {
             @ApiResponse(responseCode = "200", description = "The search was successful"),
             @ApiResponse(responseCode = "404", description = "CPF not found")
     })
-    @GetMapping("/findByCpf/{cpf}")
-    public ResponseEntity<TutorDTO.Response> findByCpf(@PathVariable String cpf) {
+    @GetMapping("/findByCpf/")
+    public ResponseEntity<?> findByCpf(@RequestParam String cpf) {
         try {
             TutorDTO.Response responseDTO = tutorService.findByCpf(cpf);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "An error occurred while searching for the tutor: "
+                                + e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
     }
 
@@ -73,15 +83,20 @@ public class TutorController {
             @ApiResponse(responseCode = "404", description = "Couldn't find this tutor to update")
     })
     @PutMapping("/update")
-    public ResponseEntity<TutorDTO.Response> update(
+    public ResponseEntity<GenericResponse<TutorDTO.Response>> update(
             @RequestParam Long id,
             @RequestBody TutorDTO.Request tutorDTO) throws Exception {
 
         TutorDTO.Response responseDTO = tutorService.update(id, tutorDTO);
+        GenericResponse<TutorDTO.Response> successResponse = new GenericResponse<>(
+                HttpStatus.OK.value(),
+                "Operação realizada com sucesso.",
+                responseDTO
+        );
         if (responseDTO == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(responseDTO);
+        return ResponseEntity.ok(successResponse);
     }
 
     @Operation(summary = "Delete a tutor", description = "Deletes a tutor in the system with the ID provided")
@@ -89,10 +104,10 @@ public class TutorController {
             @ApiResponse(responseCode = "200", description = "Tutor deleted successfully"),
             @ApiResponse(responseCode = "404", description = "Couldn't find this tutor to delete")
     })
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable Long id) {
+    @DeleteMapping("/delete/")
+    public ResponseEntity<String> deleteById(@RequestParam Long id) {
         if (!tutorService.existsById(id)) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.badRequest().body("Tutor não encontrado!");
         }
         tutorService.deleteById(id);
         return ResponseEntity.ok("Tutor deletado com sucesso!");
@@ -105,14 +120,16 @@ public class TutorController {
             @ApiResponse(responseCode = "400", description = "Pet already adopted")
     })
     @PostMapping("/adopt")
-    public ResponseEntity<String> adoptPet(
+    public ResponseEntity<?> adoptPet(
             @RequestParam Long tutorId,
             @RequestParam Long petId) {
         try {
             tutorService.adoptPet(tutorId, petId);
             return ResponseEntity.ok("Pet adotado com sucesso!");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "An error occurred while searching for the adoption: "
+                                + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
     }
 }
